ff5ab59ee34094e4d77a6f42a06b03b8aa55b914
olaftarlowski <olaf.tarlowski@gmail.com>
2019-07-22 23:30:45 +0200

    navbar menu now appears and dissappear after clicking icon, js added

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,6 @@
-console.log('red');
+// function, toggle menu from right side to 100% width
+document.getElementById('toggleMenu');
+
+let rightSideMenu = document.querySelector('.rightSideContent');
+
+document.querySelector('.toggleButton').addEventListener('click', () => { rightSideMenu.classList.toggle("showMenu")});
