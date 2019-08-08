6dfd81de2fac354c587f329b34807a24ba42b0ed
olaftarlowski <olaf.tarlowski@gmail.com>
2019-08-08 21:37:32 +0200

    gulp sass initialized

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+
+//compile scss into css
+
+function style() {
+    //source scss file
+    return gulp.src('./scss/**/*.scss')
+    //pass file to compiler
+    .pipe(sass())
+    //where to save compiled css
+    .pipe(gulp.dest('./css'))
+
+}
