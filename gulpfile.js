e5ce2d44f9462bb1f1e50f2ee90f72de81bb3008
olaftarlowski <olaf.tarlowski@gmail.com>
2019-08-09 19:32:24 +0200

    gulpfile.js added

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browserSync = require("browser-sync").create();
